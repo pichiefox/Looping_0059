667676ce6173c9ba7e428123e99a9cbcb07ad1e2
pichiefox <citrazad@gmail.com>
2026-03-31 17:06:28 +0700

    Status

--- loopingLogikaOR.cpp
+++ loopingLogikaOR.cpp
@@ -11,4 +11,10 @@ int main(){
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>nilB;
 
+    if ((rerata >= 60) || (nilM >=70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
