595acd461a8c99a5e627b9f3e5c04159b7d76c74
pichiefox <citrazad@gmail.com>
2026-03-31 17:09:54 +0700

    Status

--- loopingLogikaAND.cpp
+++ loopingLogikaAND.cpp
@@ -6,5 +6,15 @@ int main(){
     float nilB,nilM,rerata;
     string status;
 
-    
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    if ((rerata >= 60) && (nilM >=70))
+        status = "Lulus";
+    else
+        status = "Tidak lulus";
+
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 }
